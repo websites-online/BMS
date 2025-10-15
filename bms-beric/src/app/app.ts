673b038ab7885c274a3cf2e8a6707c3a34e0fbe6
danOdo <danielodobasic@hotmail.de>
2025-10-15 12:10:03 +0200

    erster entuwrf

--- bms-beric/src/app/app.ts
+++ bms-beric/src/app/app.ts
@@ -1,14 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('bms-beric');
   protected readonly currentYear = new Date().getFullYear();
+  protected readonly navOpen = signal(false);
+
+  protected toggleNav(): void {
+    this.navOpen.update((open) => !open);
+  }
+
+  protected closeNav(): void {
+    this.navOpen.set(false);
+  }
 }
