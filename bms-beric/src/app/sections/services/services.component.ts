6d9d941ad7121eb60cc6ba4cedceb91300ff109a
danOdo <danielodobasic@hotmail.de>
2025-10-22 15:35:11 +0200

    anpassungen

--- bms-beric/src/app/sections/services/services.component.ts
+++ bms-beric/src/app/sections/services/services.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RevealDirective } from '../../shared/reveal.directive';
 
 @Component({
   selector: 'app-services-section',
   standalone: true,
-  imports: [RevealDirective],
   templateUrl: './services.component.html',
   styleUrls: ['../../app.component.css']
 })
